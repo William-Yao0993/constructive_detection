419445d617e3f631ddf2cd21d67d0a4c6b8f4c12
William <u6771897@anu.edu.au>
2024-02-22 11:07:22 +1100

    batch job to server

--- train.py
+++ train.py
@@ -5,26 +5,27 @@ experiment = comet_ml.Experiment(
         project_name= "Rebar_Segmentation"
 )
 experiment.set_name(f"trial{datetime.datetime.now().date().strftime('%Y%m%d')}")
-import os 
-os.environ["OMP_NUM_THREADS"] = '8'
+# import os 
+# os.environ["OMP_NUM_THREADS"] = '8'
 
 import ultralytics
 from ultralytics import YOLO
 ultralytics.checks()
-print ("Experiment Start:", datetime.datetime.now().isoformat())
 
 #Train a model
 LOCAL_PATH = 'datasets\constructive_seg\dataset.yaml'
-SERVER_PATH = '/mnt/data/dayhoff/home/u6771897/constructive_detection/datasets/constructive_seg/data_dayhoff.yaml'
-model_path = 'seg_train_2024-01-10/train3/weights/best.pt'
-model = YOLO(model_path,task = 'segment')
+SERVER_PATH = 'datasets/panel/YOLODataset/data_dayhoff.yaml'
+
+model = YOLO(model='yolov8n-seg.pt')
 result = model.train(data=SERVER_PATH,
-                     epochs = 2000,
+                     epochs = 300,
                      lr0 = 1E-3,
                      lrf = 1E-6,
+                     optimiser = 'AdamW',
+                     save = True,
                      plots = True,
                      val = True,
+                     cache = True,
                      augment = False,
-                     project = f'seg_train_{datetime.date.today().isoformat()}'
+                     project = f'run/seg_train_{datetime.date.today().isoformat()}'
                      )
-print("Experiment End:", datetime.datetime.now())
