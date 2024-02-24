a0e5ff205ab4d1b52167a91a5de2e3b61d45434d
William Yao <u6771897@anu.edu.au>
2024-02-24 21:34:26 +1100

    update yolo panel detection

--- predict.py
+++ predict.py
@@ -1,10 +1,11 @@
 from ultralytics import YOLO
 
 img_path = 'datasets\data.png'
-model_path = 'models\seg_2024_01_10.pt'
+model_path = r'run\seg_train_2024-02-24\train11\weights\best.pt'
 model = YOLO(model_path)
 result = model.predict(
     source=img_path,
     save = True,
-    save_conf = True
+    save_conf = True,
+    project = 'runs/segment'
 )
--- train.py
+++ train.py
@@ -1,31 +1,38 @@
 import comet_ml
 import datetime
 # comet_ml.config.save(api_key = 'Qn88wPyWB3PmP0hTUAwhg1h9u')
-experiment = comet_ml.Experiment(
-        project_name= "Rebar_Segmentation"
-)
-experiment.set_name(f"trial{datetime.datetime.now().date().strftime('%Y%m%d')}")
+# experiment = comet_ml.Experiment(
+#         project_name= "Rebar_Segmentation"
+# )
+# experiment.set_name(f"trial{datetime.datetime.now().date().strftime('%Y%m%d')}")
 # import os 
 # os.environ["OMP_NUM_THREADS"] = '8'
 
-import ultralytics
+
 from ultralytics import YOLO
-ultralytics.checks()
+#ultralytics.checks()
 
-#Train a model
-LOCAL_PATH = 'datasets\constructive_seg\dataset.yaml'
-SERVER_PATH = 'datasets/panel/YOLODataset/data_dayhoff.yaml'
+def main():
+        #Train a model
+        LOCAL_PATH = 'datasets\panel\YOLODataset\dataset.yaml'
+        SERVER_PATH = r'datasets/panel/YOLODataset/data_dayhoff.yaml'
 
-model = YOLO(model='yolov8n-seg.pt')
-result = model.train(data=SERVER_PATH,
-                     epochs = 300,
-                     lr0 = 1E-3,
-                     lrf = 1E-6,
-                     optimiser = 'AdamW',
-                     save = True,
-                     plots = True,
-                     val = True,
-                     cache = True,
-                     augment = False,
-                     project = f'run/seg_train_{datetime.date.today().isoformat()}'
-                     )
+        model = YOLO(model='yolov8n-seg.pt')
+        result = model.train(data=LOCAL_PATH,
+                        epochs = 300,
+                        patience = 30,
+                        lr0 = 1E-3,
+                        lrf = 1E-6,
+                        optimizer = 'AdamW',
+                        save = True,
+                        plots = True,
+                        val = True,
+                        cache = True,
+                        augment = False,
+                        project = f'run/seg_train_{datetime.date.today().isoformat()}',
+                        device = 0
+                        )
+        
+if __name__ == '__main__':
+        #torch.multiprocessing.freeze_support()
+        main()
