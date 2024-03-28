80fa75a8ae1cdef159b9bec1f487ef5f57642fe9
William <u6771897@anu.edu.au>
2024-03-28 21:12:39 +1100

    add bboxes filtering algorithm

--- line_detect.py
+++ line_detect.py
@@ -1,9 +1,8 @@
 import cv2
 import numpy as np
-
 img_path = r'datasets\data.png'
-
-
+##---------------------------------------------------------------------------------------------------------------------------
+# Augmentation 
 def quantizeImage(img,k):
     channels = img.shape[-1]
     arr = np.float32(img).reshape(-1,channels)
@@ -27,14 +26,11 @@ def cvt_hls_inrange(img, lower, upper):
     white_mask = cv2.inRange(hls, lower, upper)
     hls_mask = cv2.bitwise_or(img,img,mask=white_mask)
     return hls_mask
-
-def extract_panel_area(img):
+def get_roi_mask(img):
     '''
-        manually extract none-panel area from image
-        Return extracted image for edges and HoughLines
-        #TODO
-        Could have extract them more wisely by different channels info???
-
+        Generate a Binary Mask 
+        Non roi -> 0
+        roi -> 1 
     '''
     height, width =  img.shape[:2]
     mask = np.ones((height,width), dtype=np.int8)
@@ -69,8 +65,25 @@ def extract_panel_area(img):
     cv2.rectangle(  mask,
                 (int(width*0.0), int(height*0.925)), 
                 (int(width*1), int(height*1.0)) ,0,-1)
-    masked_edges = cv2.bitwise_or(img,img, mask = mask)
-    return masked_edges 
+    return mask
+def extract_panel_area(img):
+    '''
+        manually extract none-panel area from image
+        Return extracted image for edges and HoughLines
+        #TODO
+        Could have extract them more wisely by different channels info???
+
+    '''
+    mask=get_roi_mask(img)
+    masked_img = cv2.bitwise_or(img,img, mask = mask)
+    return masked_img 
+# Augmentation END
+##---------------------------------------------------------------------------------------------------------------------------
+
+
+
+##-----------------------------------------------------------------------------------------------------------------
+# Canny && HoughLines Post-processing 
 def find_angles(lines):
     '''
     Return angles for HoughLinesP or HoughLine line segment
@@ -169,6 +182,125 @@ def lines_kmeans(lines,show= None):
 # cv2.resizeWindow('detection', 960, 960)   
 # cv2.imshow("detection", img)
 
+# Canny && HoughLines Post-processing END
+##-----------------------------------------------------------------------------------------------------------------
+
+##-----------------------------------------------------------------------------------------------------------------
+# YOLO Post-processing
+def get_panel_info(img):
+    '''
+    Return bboxes from YOLO prediction
+    '''
+    from ultralytics import YOLO
+    model = YOLO(r'models\panel.pt')
+    results = model(img)
+
+    return results[0].boxes.numpy()
+
+def get_orig_xyxy(bboxes):
+    xyxyn =bboxes.xyxyn
+    orig_h, orig_w = bboxes.orig_shape
+    orig_xyxy = xyxyn.copy()
+    orig_xyxy[...,0] *= orig_w
+    orig_xyxy[...,1] *= orig_h
+    orig_xyxy[...,2] *= orig_w
+    orig_xyxy[...,3] *= orig_h
+    return orig_xyxy
+def get_orig_xywh(bboxes):
+    xywhn =bboxes.xywhn
+    orig_h, orig_w = bboxes.orig_shape
+    orig_xywh = xywhn.copy()
+    orig_xywh[...,0] *= orig_w
+    orig_xywh[...,1] *= orig_h
+    orig_xywh[...,2] *= orig_w
+    orig_xywh[...,3] *= orig_h
+    return orig_xywh
+
+def bboxes_in_roi(bboxes,mask,threshold):
+    '''
+        Filter out bounding boxes if overlap ratio over threshold  
+    '''
+    orig_xyxy = get_orig_xyxy(bboxes).astype(np.int_)
+    overlap=[]
+    for x1,y1,x2,y2 in orig_xyxy:
+        area = mask[y1:y2,x1:x2]
+        overlap.append(np.sum(area)/area.size)
+    arr =np.array(overlap)
+    mask = arr > threshold
+    return bboxes[mask]
+
+def bboxes_nms(bboxes,threshold):
+
+    '''
+        Apply Non-maximum-suppression algorithm to filter out overlapping bboxes based on given IoU threshold
+         
+        Return:
+        a list of filtered bboxes in xyxy format [N*1*4]
+    '''
+    if bboxes is None:
+        return None
+    
+    # TODO
+    # O(n^2) could be imporved by better search
+    # Dymanic Programming or Hashtable ??
+    xywh= bboxes.xywh
+    bboxes_areas= xywh[...,2]*xywh[...,3] 
+    indices= bboxes_areas.argsort()[::-1] # descending order
+    orig_xyxys_sorted = get_orig_xyxy(bboxes)[indices]
+
+    orig_xyxys_sorted=orig_xyxys_sorted.tolist()
+    candidates =[]
+    while len(orig_xyxys_sorted) != 0:
+        box1 = orig_xyxys_sorted.pop()
+        candidates.append(box1)
+        for box2 in orig_xyxys_sorted:
+            if get_iou(box1,box2) > threshold:
+                orig_xyxys_sorted.remove(box2) 
+
+    arr = np.array(candidates)
+    return arr   
+
+def box_area(xyxy):
+    x1,y1,x2,y2=xyxy
+    return abs(x2-x1)*abs(y2-y1)
+def get_iou(xyxy1,xyxy2):
+
+    area1 =box_area(xyxy1)
+    area2 = box_area(xyxy2)
+
+    # IoU Anchor 
+    x1_iou = max(xyxy1[0],xyxy2[0])
+    y1_iou = max(xyxy1[1],xyxy2[1])
+    x2_iou = min(xyxy1[2],xyxy2[2])
+    y2_iou = min(xyxy1[3],xyxy2[3])
+
+    w_iou = max(0,x2_iou-x1_iou)
+    h_iou = max(0,y2_iou-y1_iou)
+    if w_iou ==0 or h_iou==0:
+        return 0.0
+    intersection = w_iou*h_iou
+    union = area1+area2 - intersection
+    return intersection/union
+
+
+# YOLO Post-processing END
+##-----------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     img = cv2.imread(img_path)
@@ -198,8 +330,9 @@ if __name__ == '__main__':
     threshold= 100
     minLineLength=55
     maxLineGap=10 
+
     #-----------------------------------------------------------------------------------------------------------------------------------------------
-    # Augmentation 
+    # Experiments Start
     #-----------------------------------------------------------------------------------------------------------------------------------------------
     # Three trials
     # hls_reduced_range = cvt_hls_inrange(img,green,white) # 1: HLS
@@ -216,8 +349,7 @@ if __name__ == '__main__':
     # edgesQuanti = cv2.Canny(blur_grayQuanti, canny_low_threshold,canny_high_threshold)
     # linesQuanti = cv2.HoughLinesP(edgesQuanti,rho=rho,theta=theta,threshold= threshold,minLineLength=minLineLength,maxLineGap=maxLineGap)
 
-
-    rgb = cv2.cvtColor(img,cv2.COLOR_BGR2RGB) # 3 No augment
+    # 3 No augment 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     blur_gray = cv2.GaussianBlur(gray,(kernel_size),0)
     edges = cv2.Canny(blur_gray, canny_low_threshold,canny_high_threshold)
@@ -225,15 +357,24 @@ if __name__ == '__main__':
     lines = cv2.HoughLines(edges,rho,theta,threshold)
     orthgonal_lines = degree_filter(lines,5)
     orthgonal_linesP = degree_filter(linesP,5)
-    print(orthgonal_lines.shape)
-    print(orthgonal_linesP.shape)
+    
+    
+    # use panel info as line segments length 
+    bboxes = get_panel_info(img_path)
+    overlap = 0.8 # threshold for bboxes in non-roi 
+    bboxes = bboxes_in_roi(bboxes,get_roi_mask(img), overlap)
+    orig_xyxys= bboxes_nms(bboxes,threshold=0.1)
+    for x1,y1,x2,y2 in orig_xyxys.astype(np.int_):
+        p1= x1,y1
+        p2= x2,y2
+        cv2.rectangle(img,p1,p2,(0,0,255),3,0)
+    
+    # TODO: 
+    # use rho,theta to draw segment within panel 
+    # use line segments from HoughLinesP to verify above and connect in-between gap
+    cv2.namedWindow('Result',cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Result',980,980)
+    cv2.imshow('Result',img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     
-    # TODO: 
-    # use panel info as line segments length 
-    # use rho,theta to draw segment within panel 
-    # use line segments from HoughLinesP to verify above and connect in-between gap
-
-    
-     
